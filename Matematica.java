36f4517eb6506019400d10b90621155ec6a53a10
Beatriz Aimi <biagioni.aimi@gmail.com>
2023-04-11 11:52:57 -0300

    Add files via upload

---
+++ Matematica.java
@@ -0,0 +1,15 @@
+package dogss;
+
+public class Matematica {
+	
+	int maior (int um, int dois) {
+		if (um > dois) {
+			return um;
+		}else {
+			return dois;
+		}
+		
+	}
+	
+
+}
---
+++ MatematicaTeste.java
@@ -0,0 +1,14 @@
+package dogss;
+
+public class MatematicaTeste {
+
+	public static void main(String[] args) {
+		
+		
+		Matematica m = new Matematica();
+		int maior = m.maior(20,30);
+		System.out.println(maior);
+		
+	}
+
+}
